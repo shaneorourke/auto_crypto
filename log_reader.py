d975a328e553349fde631f08d1756b0804027062
shaneorourke <shaneorourke110@gmail.com>
2023-02-17 21:34:51 +0000

    Update log_reader.py

--- log_reader.py
+++ log_reader.py
@@ -113,7 +113,7 @@ if __name__ == '__main__':
         print(f'Long Profit:{round(LONG_CLOSED_TP_PROFIT,2)}')
         print(f'Short Profit:{round(SHORT_CLOSED_PROFIT,2)}')
         print(f'Long Losses:{round(LONG_CLOSED_SL_LOSS,2)}')
-        print(f'Short Losses:{round(SHORT_CLOSED_PROFIT,2)}')
+        print(f'Short Losses:{round(SHORT_CLOSED_SL_LOSS,2)}')
         print()
 
     print(f'Total Profit:{round(total_profit,2)}')
